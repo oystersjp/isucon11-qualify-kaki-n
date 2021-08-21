97f2d0c76cb63d5fb2fd2784b06973062a333175
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2021-08-21 11:39:13 +0900

    chore: アクセスログがうるさい

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -212,7 +212,7 @@ func main() {
 	e.Debug = false
 	e.Logger.SetLevel(log.DEBUG)
 
-	e.Use(middleware.Logger())
+	//e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	e.POST("/initialize", postInitialize)
