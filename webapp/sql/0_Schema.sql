7a4e9761f21becb8dfb2b49d3d5b20fcdb04cad3
cureseven <shiningcureseven@gmail.com>
2021-08-21 11:31:14 +0900

    index追加

--- webapp/sql/0_Schema.sql
+++ webapp/sql/0_Schema.sql
@@ -27,6 +27,7 @@ CREATE TABLE `isu_condition` (
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 alter table isu_condition add index (jia_isu_uuid);
+alter table isu_condition add index (jia_isu_uuid, minus_timestamp);
 
 CREATE TABLE `user` (
   `jia_user_id` VARCHAR(255) PRIMARY KEY,
