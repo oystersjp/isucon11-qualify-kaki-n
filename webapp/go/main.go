3b8b4c7db60057ce9242fc24821acd1e2fb23286
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2021-08-21 11:45:58 +0900

    fix: uooooo

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1194,13 +1194,24 @@ func BulkInsertIsuCondition() {
 
 	log.Print("BulkInsertIsuCondition: 頑張るぞー")
 
+	var insertMap []map[string]interface{}
+	for i, v := range inserts {
+		insertMap[i] = map[string]interface{}{
+			"JiaIsuUUID": v.JiaIsuUUID,
+			"Timestamp":  v.Timestamp,
+			"IsSitting":  v.IsSitting,
+			"Condition":  v.Condition,
+			"Message":    v.Message,
+		}
+	}
+
 	q := `
 		INSERT INTO isu_condition (jia_isu_uuid, timestamp, is_sitting, condition, message)
 		VALUES (:JiaIsuUUID, :Timestamp, :IsSitting, :Condition, :Message)
 	`
 
 	tx := db.MustBegin()
-	if _, err := tx.NamedExec(q, inserts); err != nil {
+	if _, err := tx.NamedExec(q, insertMap); err != nil {
 		log.Print("error", err)
 		_ = tx.Rollback()
 
